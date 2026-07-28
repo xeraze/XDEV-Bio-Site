0d91d3991b6320589f01b9c80d8a97a8500db838
xeraze <xeraze@example.com>
2026-07-28 13:24:43 +0300

    First Release

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://xeraze.github.io',
+  base: '/XDEV-Bio-Site',
+  compressHTML: true,
+});
---
+++ src/lib/projects.config.ts
@@ -0,0 +1,13 @@
+import type { ProjectConfig } from '../types';
+
+export const projects: ProjectConfig[] = [
+  { repo: 'xeraze/KBank', displayName: 'KredaBank Site' },
+  { repo: 'xeraze/tsn-ua', displayName: 'TNS Site' },
+  { repo: 'xeraze/Pricelist', displayName: 'Pricelist Site' },
+  { repo: 'xeraze/XDEV-Bio-Site', displayName: 'This Site!' },
+  { repo: 'xeraze/SH-Site', displayName: 'Sloboda Hospital Help Center' },
+  { repo: 'xeraze/XNote', displayName: 'A simple notes app' },
+];
+
+export const GITHUB_USERNAME = 'xeraze';
+export const DISCORD_USER_ID = '1404735389603860503';
