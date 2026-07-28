3adfc010628dd757b6cd1e8c786b48f25ffe4c6f
xeraze <xeraze@example.com>
2026-07-28 13:36:54 +0300

    Some Changes in Projects List

--- src/lib/projects.config.ts
+++ src/lib/projects.config.ts
@@ -1,12 +1,12 @@
 import type { ProjectConfig } from '../types';
 
 export const projects: ProjectConfig[] = [
-  { repo: 'xeraze/KBank', displayName: 'KredaBank Site' },
-  { repo: 'xeraze/tsn-ua', displayName: 'TNS Site' },
-  { repo: 'xeraze/Pricelist', displayName: 'Pricelist Site' },
+  { repo: 'xeraze/KBank' },
+  { repo: 'xeraze/tsn-ua', displayName: 'TNS WebSite' },
+  { repo: 'xeraze/Pricelist', displayName: 'My Pricelist Site!' },
   { repo: 'xeraze/XDEV-Bio-Site', displayName: 'This Site!' },
-  { repo: 'xeraze/SH-Site', displayName: 'Sloboda Hospital Help Center' },
-  { repo: 'xeraze/XNote', displayName: 'A simple notes app' },
+  { repo: 'xeraze/SH-Site' },
+  { repo: 'xeraze/XNote' },
 ];
 
 export const GITHUB_USERNAME = 'xeraze';
